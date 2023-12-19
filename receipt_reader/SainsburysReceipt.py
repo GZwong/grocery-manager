9d5d760e1c5c5ddf860389183f6bd164357c5b7e
Gai <wonggaizhe@gmail.com>
2023-12-19 20:24:55 +0000

    update/receipt data format
    Updated the storage method of receipt data from a dataframe to a list of dictionaries for modularity.

--- receipt_reader/SainsburysReceipt.py
+++ receipt_reader/SainsburysReceipt.py
@@ -11,23 +11,15 @@ class SainsburysReceipt():
         self._file = pdf_file
         
         self._content = None           # A list of PDF lines (Raw)
-        self._filtered_content = None  # A list of strings for items
         self._order_id = None          # Order ID
         self._order_date = None        # Order Date
-        self._quantities = None        # List of quantities for each item
-        self._weights = None           # List of weights for each item
-        self._names = None             # List of names for each item
-        self._prices = None            # List of prices for each item
+        self._item_dict = None         # List of dictionaries containing info for each item
 
-        self._item_df = None           # Pandas Dataframe of all orders
-        
         self._parse_receipt()
         # Order ID and time
         self._find_order_id_time()
         # Order Items
-        self._filter_content_to_items()
         self._find_items_info()
-        self._process_item_info()
   
     def _parse_receipt(self):
         """
@@ -79,30 +71,13 @@ class SainsburysReceipt():
         # Save permanently as attributes
         self._order_id = order_id
         self._order_date = order_date
-        
-
-    def _filter_content_to_items(self):
-        """
-        Remove unnecessary information within the receipt besides the orders. This is located between
-        "Delivery summary" and "Order summary".
-        """
-        # The order starts after the line "Delivery summary" and ends after "Order summary". 
-        # Only retain whatever is in between.
-        for index, line in enumerate(self._content):
-            if line.startswith("Delivery summary"):
-                start_index = index
-            elif line.startswith("Order summary"):
-                end_index = index
-        
-        self._filtered_content = self._content[start_index + 1: end_index]
-
 
     def _find_items_info(self):
         """
-        With a filtered list (e.g. returned from find_orders(content_list)), decouple each line into
-        the "Quantity", "Item" and "Price" of each Item.
+        Decouple each item into its "Name", "Quantity", "Weight" and "Price",
+        then store them as a list of dictionaries.
 
-        Logic:
+        Implementation Logic:
             1. The "amount" of an item is either:
                 - Quantity
                 - Weight
@@ -112,16 +87,20 @@ class SainsburysReceipt():
             4. For long orders (multi-lines) check whether the £ symbol appears. If it does not, append it to the next line
         """
 
-        # Initialize lists
-        quantities = []
-        weights = []
-        names = []
-        prices = []
+        # Filter content to information on the orders only
+        for index, line in enumerate(self._content):
+            if line.startswith("Delivery summary"):
+                start_index = index
+            elif line.startswith("Order summary"):
+                end_index = index
+        
+        filtered_content = self._content[start_index + 1: end_index]
 
+        # Initialize lists
         previous_line = ''         # In case a single item spans multiple rows. See logic below.
         previous_line_length = 0   # To adjust pound index in case of multi-line rows
         
-        for order in self._filtered_content:
+        for order in filtered_content:
 
             # Find the index of the pound symbol. Everything after this is assumed to be the price.      
             pound_index = order.rfind('£')
@@ -161,70 +140,25 @@ class SainsburysReceipt():
                 amount = order[: item_index]
                 name = order[item_index: pound_index - 1]
                 price = order[pound_index + 1:]
-                
             
                 # Amount can either be quantity or weight. Store it as 'weight' if it ends with 'kg'.
-                # Add other units in the future.
+                # TODO: Add other units in the future.
                 if amount.endswith("kg"):
                     weight = amount
                     quantity = None
                 else:
                     weight = None
-                    quantity = int(amount)             # Type conversion here to prevent performing `int(None)``
+                    quantity = amount
 
-                # Append to each list
-                quantities.append(quantity)            # Quantity stored as integers
-                weights.append(weight)                 # Weight stored as strings
-                names.append(name)                     # Item names stored as strings
-                prices.append(float(price))            # Price stored as floats
-
-        self._quantities = quantities
-        self._weights = weights
-        self._names = names
-        self._prices = prices
-    
-    
-    def _process_item_info(self):
-        """
-        With the quantities, weights, names and prices of each item, process the data such that each item
-        always take one row (an item with a quantity of two will become two items of one quantity each).
-        This is to ensure each item can be split separately.
-        """
-        
-        # For rows with a quantity above one (quantity = n), split this into 
-        # n rows and divide the price by n to obtain individual prices
-        # ----------
-        
-        # New lists to store the decoupled information
-        decoupled_weights = []
-        decoupled_items = []
-        decoupled_prices = []
-        
-        for quantity, weight, item, price in zip(self._quantities, self._weights, self._names, self._prices):
-            # When quantity exceeds 1, split that order into individual items
-            if (quantity) and (quantity > 1):
-                decoupled_weights.extend([weight] * quantity)
-                decoupled_items.extend([item] * quantity)
-                # Prices are summed so these are divided by quantity
-                decoupled_prices.extend([price/quantity] * quantity)
-            
-            # If quantity is one then just append it normally
-            else:
-                decoupled_weights.append(weight)
-                decoupled_items.append(item)
-                decoupled_prices.append(price)
-                
-        # Convert to dataframe as a viewable form
-        self._item_df = pd.DataFrame(
-            {
-                # Append the order_id (which is equal throughout) for referencing other datatables
-                'order_id': [self._order_id] * len(decoupled_items),
-                'weight': decoupled_weights,
-                'item_name': decoupled_items,
-                'price': decoupled_prices
-            }
-        )
-    
+                # Append information to the list of dictionaries
+                self._item_dict.append(
+                    {
+                        "Name": name,
+                        "Quantity": int(quantity),
+                        "Weight": weight,
+                        "Price": float(price)
+                    }
+                )
     
     @property
     def order_id(self):
@@ -235,6 +169,6 @@ class SainsburysReceipt():
         return self._order_date
     
     @property
-    def item_df(self):
-        return self._item_df
+    def items(self):
+        return self._item_dict
     
